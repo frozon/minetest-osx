7ddd74b0f675d7182a4b570a623944e66759526e
Thomas <frozon@gmail.com>
2012-07-25 15:48:11 +0200

    Initial xcode project commit

---
+++ minetest-osx/minetest.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#  minetest.sh
+#  minetest-osx
+#
+#  Created by Thomas Lauro on 25/07/12.
+#  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+
+tmp="`pwd`/$0"
+bundle=`dirname "$tmp"`
+exec "$bundle/minetest-osx"
